72f749025214458bd96d251f3da634f72297bd2d
hansaeging <93020146+hansaeging@users.noreply.github.com>
2022-05-21 14:58:23 +0900

    Add files via upload

---
+++ input_ex.py
@@ -0,0 +1,5 @@
+name = input('이름을 입력하세요:')
+print(name)
+
+x = int(input("숫자를 입력하세요:"))
+print(x + 5)
---
+++ library_ex.py
@@ -0,0 +1,16 @@
+import math
+print(math.log10(1000))
+print(math.log2(8))
+print(math.sin(60))
+print(math.pi)
+
+import random
+print(random.random())
+print(random.randint(1, 45))
+print(random.uniform(0, 1))
+
+import os
+print(os.getlogin())
+print(os.getcwd())
+
+
